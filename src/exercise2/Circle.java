330490f34e7f2b2159461a51181fcd92296a6faf
Thrace123 <72183930+Thrace123@users.noreply.github.com>
2020-10-07 06:12:55 +0000

    Aufgabe 2

---
+++ src/exercise2/Circle.java
@@ -0,0 +1,18 @@
+package exercise2;
+
+import java.util.Scanner;
+
+public class Circle {
+public static void main(String[] args) {
+System.out.println("r:");
+        Scanner input = new Scanner(System.in);
+
+
+  int r = input.nextInt();
+  double п = 3.14;
+  double C = (2.0*п*r);
+  double A = (п*r);
+System.out.println("circumstance"+ C);
+System.out.println("Area" + A);
+}
+}
