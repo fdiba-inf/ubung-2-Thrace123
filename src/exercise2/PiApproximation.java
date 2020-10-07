7c5afaaac8f2fc2064f2d4b7212efa02b81a327e
Thrace123 <72183930+Thrace123@users.noreply.github.com>
2020-10-07 05:49:17 +0000

    number

---
+++ src/exercise2/PiApproximation.java
@@ -0,0 +1,18 @@
+package exercise2;
+
+
+
+public class PiApproximation {
+
+
+public static void main(String[] args) {
+     
+   double number = 1.0- 1.0/3.0 + 1.0/5.0 - 1.0/7.0 + 1.0/11.0 - 1.0/13.0 + 1.0/17.0 - 1.0/19.0 + 1.0/23.0; 
+   double number2 = 4.0;
+   double number3 = (number*number2);
+   System.out.println("number3"+ number3);
+}
+
+
+
+}
